eb9ec908863206494b0e99d5cf45192bd4994b76
cr-zhichen <zgccrui@outlook.com>
2024-01-15 21:28:41 +0800

    加入目录复制功能，在保持目录结构复制的同时，如果图片Hash相同，则修改新文件名加入Hash

---
+++ EXIFReduction/CopyDirectory.cs
@@ -0,0 +1,100 @@
+﻿using System.Security.Cryptography;
+
+namespace EXIFReduction;
+
+public class CopyDirectory
+{
+    private static DateTime _startTime;
+    private static int _totalFiles;
+
+    public static void Start(string directory, string targetDirectory, int maxDegreeOfParallelismString)
+    {
+        Console.WriteLine("是否开始处理？(Y/N) 默认为 N");
+        string? input = Console.ReadLine();
+        if (input != "Y" && input != "y")
+        {
+            Console.WriteLine("按任意键退出");
+            return;
+        }
+
+        CopyFile(directory, targetDirectory, maxDegreeOfParallelismString);
+
+        Console.WriteLine($"处理完成，共处理 {_totalFiles} 个文件，耗时 {DateTime.Now - _startTime}");
+    }
+
+    private static void CopyFile(string directory, string targetDirectory, int maxDegreeOfParallelismString)
+    {
+        // 获取所有文件(包括子文件夹)
+        var allFiles = Directory.EnumerateFiles(directory, "*.*", SearchOption.AllDirectories).ToList();
+        _startTime = DateTime.Now;
+        _totalFiles = allFiles.Count;
+        int currentFile = 0;
+
+        // 将所有文件复制到目标目录（包括相对的目录结构）
+        // 例如：源路径为：D:\test\ 目标路径为：E:\test\  获取到的文件为：D:\test\2022\1.jpg 则复制到：E:\test\2022\1.jpg
+        Parallel.ForEach(allFiles, new ParallelOptions
+        {
+            MaxDegreeOfParallelism = maxDegreeOfParallelismString
+        }, file =>
+        {
+            string targetFile = file.Replace(directory, targetDirectory);
+            string targetFileDirectory = Path.GetDirectoryName(targetFile) ?? "";
+            if (!Directory.Exists(targetFileDirectory))
+            {
+                Directory.CreateDirectory(targetFileDirectory);
+            }
+
+            // 判断文件是否存在，存在则判断hash是否相同，如果哈希相同则跳过，如果哈希不同则在文件名后面加上hash值
+            try
+            {
+                if (File.Exists(targetFile))
+                {
+                    byte[] sourceFileHash = ComputeFileHash(file);
+                    byte[] targetFileHash = ComputeFileHash(targetFile);
+                    if (sourceFileHash.SequenceEqual(targetFileHash))
+                    {
+                        currentFile++;
+                        UpdateProgress(currentFile, _totalFiles);
+                        return;
+                    }
+
+                    string fileName = Path.GetFileNameWithoutExtension(targetFile);
+                    string extension = Path.GetExtension(targetFile);
+                    targetFile = Path.Combine(targetFileDirectory,
+                        $"{fileName}_{BitConverter.ToString(sourceFileHash).Replace("-", "")}{extension}");
+                }
+
+                File.Copy(file, targetFile);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+
+            currentFile++;
+            UpdateProgress(currentFile, _totalFiles);
+        });
+    }
+
+    private static void UpdateProgress(int currentFile, int totalFiles)
+    {
+        double percent = (double)currentFile / totalFiles * 100;
+        TimeSpan elapsed = DateTime.Now - _startTime;
+
+        // 估计总时间
+        TimeSpan estimatedTotal = TimeSpan.FromTicks((long)(elapsed.Ticks / (percent / 100)));
+        // 预计剩余时间
+        TimeSpan remaining = estimatedTotal - elapsed;
+
+        Console.Write(
+            $"\rProgress: {currentFile}/{totalFiles} files processed ({percent:N2}%) - Estimated Time Remaining: {remaining:hh\\:mm\\:ss}");
+    }
+
+    private static byte[] ComputeFileHash(string file)
+    {
+        using var md5 = MD5.Create();
+        using var stream = File.OpenRead(file);
+        return md5.ComputeHash(stream);
+    }
+}
--- EXIFReduction/Program.cs
+++ EXIFReduction/Program.cs
@@ -23,21 +23,15 @@ Console.WriteLine("请输入线程数(默认为5): ");
 string? threadCount = Console.ReadLine();
 int threadCountInt = !string.IsNullOrWhiteSpace(threadCount) ? int.Parse(threadCount) : 5;
 
-// 0-全部执行
 // 1-EXIF信息恢复
 // 2-文件夹结构创建
-Console.WriteLine("请输入操作类型0-全部执行\n1-EXIF信息恢复\n2-文件夹结构创建\n(默认为1): ");
+// 3-目录复制
+Console.WriteLine("请输入操作类型0-全部执行\n1-EXIF信息恢复\n2-文件夹结构创建\n3-目录复制\n(默认为1): ");
 string? operationType = Console.ReadLine();
 int operationTypeInt = !string.IsNullOrWhiteSpace(operationType) ? int.Parse(operationType) : 1;
 
 switch (operationTypeInt)
 {
-    case 0:
-        // 进行EXIF信息的恢复
-        ExifReduction.Start(directory, threadCountInt);
-        // 进行文件夹结构的创建
-        CreateFolderStructure.Start(directory, threadCountInt);
-        break;
     case 1:
         // 进行EXIF信息的恢复
         ExifReduction.Start(directory, threadCountInt);
@@ -45,6 +39,18 @@ switch (operationTypeInt)
     case 2:
         // 进行文件夹结构的创建
         CreateFolderStructure.Start(directory, threadCountInt);
+        break;
+    case 3:
+        Console.WriteLine("请输入目标目录: ");
+        string targetDirectory = Console.ReadLine() ?? "";
+        if (string.IsNullOrWhiteSpace(targetDirectory))
+        {
+            Console.WriteLine("目标目录不能为空");
+            return;
+        }
+
+        // 进行目录复制
+        CopyDirectory.Start(directory, targetDirectory, threadCountInt);
         break;
     default:
         Console.WriteLine("操作类型错误");
